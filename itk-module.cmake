54e128202ef1d5448f833f7735b61771f0db5aaf
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-11-11 08:35:50 -0500

    STYLE: Conform to `CMake` style.
    Conform to `CMake` style:
    - Remove unnecessary white space at the beginning.
    - Remove `CMake` white spaces before/after `CMake` arguments in `itk-module.cmake`.

--- itk-module.cmake
+++ itk-module.cmake
@@ -1,15 +1,14 @@
-
 # the top-level README is used for describing this module, just
 # re-used it for documentation here
-get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
+get_filename_component(MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURENT_DIR}/README" DOCUMENTATION)
 
 
 # The testing module depends on ITKTestKernel)
  
 # define the dependencies of the include module and the tests
 itk_module(RobustPredicate
-DEPENDS
+  DEPENDS
   TEST_DEPENDS
     ITKTestKernel
   EXCLUDE_FROM_DEFAULT
